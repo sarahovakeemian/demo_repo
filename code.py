a4a9632ea27c6309c00cb1dbda8ef0772ef45cef
sarahovakeemian <sarahovakeemian>
2025-10-21 16:15:13 +0000

    testing

--- code.py
+++ code.py
@@ -3,4 +3,11 @@ print('hello world. New feature added.')
 
 # COMMAND ----------
 
+import time
+for i in range(1, 5):
+  time.sleep(10)
+  print (f'hello, printing {i}')
+
+# COMMAND ----------
+
 
